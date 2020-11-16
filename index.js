134ac13b23da252a095cb46fe49a6e8050e094b2
rodriguesluis <rodriguesdasilvaluis@gmail.com>
2020-11-16 15:53:12 +0100

    exo modifier

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 document.querySelector('button').addEventListener("click",function(){
-    fetch("http://api.icndb.com/jokes/random")
+    fetch("https://api.icndb.com/jokes/random")
     .then( response => response.json())
     .then( (data) => {
         console.log(data);
